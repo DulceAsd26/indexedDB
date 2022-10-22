9340f1176a36deeecb8f06ca3adfc02220f1fb44
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-21 23:45:24 -0500

    Manejo de errores e inserción de registros

--- app.js
+++ app.js
@@ -15,5 +15,46 @@ request.onupgradeneeded = event => {
 
 };
 
+//Manejo de errores
+request.onerror = event => {
+    console.log('DB error', event.target.error );
+};
+
+
+//Insertar datos
+request.onsuccess = event => {
+
+    let db = event.target.result;
+
+    let heroesData = [
+        {id: '1111', heroe: 'Spiderman', mensaje: 'Aquí su amigo Spiderman'},
+        {id: '2222', heroe: 'Ironman', mensaje: 'Aquí en mi nuevo Mark 50'},
+    ];
+
+
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+
+    heroesTransaction.onerror = event => {
+        console.log('Error guardado', event.target.error);
+    };
+
+    //Informa sobre ek éxito de la tranacción
+    heroesTransaction.oncomplete = event => {
+        console.log('Transacción hecha', event);
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for( let heroe of heroesData ){
+        heroesStore.add( heroe);
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregando a la base de datos');
+    };
+
+
+};
+
 
 
