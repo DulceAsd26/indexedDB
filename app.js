044a1ccf5df827dda42036c9a316481ff1dfd44a
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-21 23:07:53 -0500

    Iniciando indexeddb

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 
 // indexedDB: Reforzamiento
+let request = window.indexedDB.open('mi-database', 1);
 
+//Se actualiza cuando se crea o se sube la versiónd e la base datos
+request.onupgradeneeded = event => {
+
+    console.log('Actualización de BD');
+
+    let db = event.target.result;
+
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+
+};
 
 
 
